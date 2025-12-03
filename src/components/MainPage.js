911e9712596af4ac70fbf0ebee3bf13f0fa30506
Bereket Kibret <bekibret@microsoft.com>
2025-12-02 20:42:16 -0800

    build fix

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import GoogleReviews from './GoogleReviews';
 import ServiceCard from './ServiceCard';
 import ContactForm from './ContactForm';
